492afdd5abe4ca770c70ffecb15e70a5874859e6
John Guignard <john.guignard@gmail.com>
2016-10-16 01:37:30 +0200

    Add reducers data samples

---
+++ app/reducers/data/beneficiary.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    'id': 1,
+    'matricule': 'B245876463',
+    'nom': 'Dupond',
+    'prenom': 'Etnan'
+  },
+  {
+    'id': 2,
+    'matricule': 'B245525763',
+    'nom': 'Durand',
+    'prenom': 'Dufond'
+  },
+  {
+    'id': 3,
+    'matricule': 'B245874577',
+    'nom': 'Aller',
+    'prenom': 'Plusloin'
+  }
+];
---
+++ app/reducers/data/pays.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    'id': 1,
+    'desc': 'Allemagne'
+  },
+  {
+    'id': 2,
+    'desc': 'Espagne'
+  },
+  {
+    'id': 3,
+    'desc': 'France'
+  }
+];
