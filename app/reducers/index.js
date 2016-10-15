021b339088e2f6f70d2d52ed1d1a8aa406ec3dd4
Loïc C <loic.carney@gmail.com>
2016-10-15 19:09:07 +0200

    Renommage du dépanneur en acheteur

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import authentication from './authentication';
 import authorization from './authorization';
 import managementSidebarMenu from './managementSidebarMenu';
-import depanneurSidebarMenu from './depanneurSidebarMenu';
+import acheteurSidebarMenu from './acheteurSidebarMenu';
 import assureurSidebarMenu from './assureurSidebarMenu';
 import clientSidebarMenu from './clientSidebarMenu';
 import status from './status';
@@ -12,7 +12,7 @@ const jammyApp = combineReducers({
   authorization,
   authentication,
   managementSidebarMenu,
-  depanneurSidebarMenu,
+  acheteurSidebarMenu,
   assureurSidebarMenu,
   clientSidebarMenu,
   status,
